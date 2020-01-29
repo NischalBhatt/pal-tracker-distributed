1e8ebeaf377e15609fb25a2e9f0f74a1016e469e
PAL-1 <amittttmishra@gmail.com>
2020-01-29 16:59:40 +0530

    Service Discovery changes

--- Applications/AllocationsServer/Startup.cs
+++ Applications/AllocationsServer/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Pivotal.Discovery.Client;
+using Steeltoe.Common.Discovery;
 
 namespace AllocationsServer
 {
@@ -24,19 +26,22 @@ namespace AllocationsServer
         {
             // Add framework services.
             services.AddMvc();
+            services.AddDiscoveryClient(Configuration);
 
             services.AddDbContext<AllocationContext>(options => options.UseMySql(Configuration));
             services.AddScoped<IAllocationDataGateway, AllocationDataGateway>();
 
             services.AddSingleton<IProjectClient>(sp =>
             {
-                var httpClient = new HttpClient
+                var handler = new DiscoveryHttpClientHandler(sp.GetService<IDiscoveryClient>());
+                var httpClient = new HttpClient(handler, false)
                 {
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
                 return new ProjectClient(httpClient);
             });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +51,7 @@ namespace AllocationsServer
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            app.UseDiscoveryClient();
         }
     }
 }
